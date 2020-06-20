a5f0124564f5824c1fca5e74172ba9bb40eb15c4
Hieu Minh Le <6100252-hieucoder@users.noreply.gitlab.com>
2020-06-21 04:58:57 +0700

    Created a new MS SQL Server BookStore database and implemented data classes.

---
+++ BookStore-API/Data/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore_API.Data
+{
+    [Table("Authors")]
+    public partial class Author
+    {
+        public int Id { get; set; }
+        [Column("First Name")]
+        public string FirstName { get; set; }
+        [Column("Last Name")]
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public virtual IList<Book> Books { get; set; }
+    }
+}
